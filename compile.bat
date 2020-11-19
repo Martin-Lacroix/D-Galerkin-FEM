e729a6c64c9efaa343345fc2e3787829cae9d5ad
Martin-Lacroix <lacroix.martin@protonmail.com>
2020-11-19 13:15:27 +0100

    compile.bat

---
+++ compile.bat
@@ -0,0 +1,3 @@
+mkdir build
+xcopy gmsh-4.1.dll build\
+g++ -O2 source\configParser.cpp source\utils.cpp source\Mesh.cpp source\solver.cpp source\dgalerkin.cpp -fopenmp -I C:\ProgramData\Eigen -L include\ -lgmsh -o build\dgalerkin.exe
