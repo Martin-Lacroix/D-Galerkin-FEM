b40442b2f85ee8265192f385c4f81ba60284397c
Martin Lacroix <47146335+Martin-Lacroix@users.noreply.github.com>
2021-07-11 11:26:54 +0200

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,5 +1,5 @@
 mkdir output
 xcopy gmsh-4.1.dll output\
 
-set path-eigen=C:\Computing\msys64\mingw64\include\eigen3
+set path-eigen=C:\Local\msys64\mingw64\include\eigen3
 g++ -O2 source\*.cpp -fopenmp -I %path-eigen% -L include\ -lgmsh -o output\dgalerkin.exe
